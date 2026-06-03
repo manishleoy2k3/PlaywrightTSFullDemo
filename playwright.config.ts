5a4de9d5c660804f44066f135f538d0cdd89d22d
manishleoy2k3 <manishleoy2k3@gmail.com>
2026-06-03 10:03:39 +0530

    updated yml file for Manual run

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,14 +30,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
+  reporter: [['blob']],
+  /*
+  [
     ['html'],
     ['list'],
     //['dot'],
     ['json', { outputFile: 'json-test-report.json' }],
     ['junit', { outputFile: 'junit-test-report.xml' }],
     ['allure-playwright'],
-  ],
+  ],*/
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
